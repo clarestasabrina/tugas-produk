3d304ae9f5b4b21c7eaf5e1a4311c69725d6ab0b
clarestasabrina <clarerasintiya@gmail.com>
2024-12-11 14:17:26 +0700

    Add files via upload

---
+++ lib/models/api.dart
@@ -0,0 +1,7 @@
+class BaseUrl {
+  static String tambah = "http://10.2.17.62/store/controller/insert.php";
+  static String data = "http://10.2.17.62/store/controller/list.php";
+  // static String lihat = "http://192.168.1.103/produk/details.php";
+  static String edit = "http://10.2.17.62/store/controller/update.php";
+  static String hapus = "http://10.2.17.62/store/controller/delete.php";
+}
